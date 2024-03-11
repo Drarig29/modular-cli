1aee56ca638492851322e845a36c95dd7cf99163
corentin <corentin.girard@datadoghq.com>
2024-03-11 19:34:05 +0100

    Release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,9 +3,9 @@ sed -i -e "s/\"version\": \".*\"/\"version\": \"$1\"/" package.json packages/*/p
 
 git add .
 git commit -m "$1"
+git push
 
 yarn workspaces list --json | jq -r '.location' | while read -r dir; do
   cd $(git rev-parse --show-toplevel)/$dir
-
-  echo "publish"
+  npm publish --access public
 done
