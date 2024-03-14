3bb67c193cce5626b0c49b44b4d9b80bd5dfef5e
corentin <corentin.girard@datadoghq.com>
2024-03-14 16:49:36 +0100

    Update release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,5 +1,6 @@
 # Replace version in all `package.json`
 sed -i -e "s/\"version\": \".*\"/\"version\": \"$1\"/" package.json packages/*/package.json
+sed -i -e "s/const VERSION = \".*\"/const VERSION = \"$1\"/" src/cli.js
 
 git add .
 git commit -m "$1"
@@ -9,3 +10,14 @@ yarn workspaces list --json | jq -r '.location' | while read -r dir; do
   cd $(git rev-parse --show-toplevel)/$dir
   npm publish --access public
 done
+
+git checkout dist
+git merge main
+
+# Build dist files
+yarn build
+git add .
+git commit -m "Commit dist files"
+git tag @drarig29/modular-cli/v$1
+git push
+git push --tags
